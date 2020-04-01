76281cec84fcb6cedc1e68a4c6d549ced46305cc
harsha764 <hreddy710@gmail.com>
2020-04-01 19:28:05 +0530

    build files updated

---
+++ build/precache-manifest.43e74811eb1269923e4cc2065bb8194b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bd6cb2c573f742677259ecd3a59fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7d6ab7b7a238c176f8",
+    "url": "/static/css/2.fab18f34.chunk.css"
+  },
+  {
+    "revision": "311da6eb5bb460292828",
+    "url": "/static/css/main.aad42995.chunk.css"
+  },
+  {
+    "revision": "da7d6ab7b7a238c176f8",
+    "url": "/static/js/2.483b3e30.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.483b3e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311da6eb5bb460292828",
+    "url": "/static/js/main.90460ed6.chunk.js"
+  },
+  {
+    "revision": "7c4b1b2fc3f74d00f8e8",
+    "url": "/static/js/runtime-main.fd303d24.js"
+  }
+]);
